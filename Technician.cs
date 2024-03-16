2dad5fd823ba16ffc576f94788f6a1cb94f21dd1
mTay829 <84362028+mTay829@users.noreply.github.com>
2024-03-16 16:14:40 -0400

    Got the technicians to display the first and last name. they all pop up not just the ones that can perform the service

--- Technician.cs
+++ Technician.cs
@@ -1,25 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace MechanicShop
 {
     public partial class Technician : Form
     {
+        private const string connectionString = "Server=localhost\\SQLEXPRESS;Database=MechanicShop;Integrated Security=SSPI;";
+
         public Technician()
         {
             InitializeComponent();
+            PopulateTechniciansComboBox();
         }
+
+        private void PopulateTechniciansComboBox()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "SELECT Tech_ID, Tech_FN, Tech_LN FROM Technician";
+                    SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+
+                    dt.Columns.Add("FullName", typeof(string), "Tech_FN + ' ' + Tech_LN"); // Concatenate first name and last name into FullName column
+
+                    comboBox1.DisplayMember = "FullName"; // Use FullName as the DisplayMember
+                    comboBox1.ValueMember = "Tech_ID";
+                    comboBox1.DataSource = dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
 
         private void Technician_Load(object sender, EventArgs e)
         {
-
+            PopulateTechniciansComboBox();
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
